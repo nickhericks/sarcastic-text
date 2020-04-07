1c868fd5aac415061b0069b16a92711283d7b6d6
Nick-Hericks <nickhericks@gmail.com>
2020-04-07 11:47:11 -0500

    Add funky filter

--- text.js
+++ text.js
@@ -7,7 +7,7 @@ const funkyLetters = {
   '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ'
 };
 
-
+console.log(Object.keys(funkyLetters));
 const filters = {
   sarcastic(letter, index) {
     if (index % 2 == 0) {
@@ -18,9 +18,15 @@ const filters = {
     return letter.toLowerCase();
   },
   funky(letter) {
-
-
-		
+		// check if there is a funky letter for the character
+		const funkyLetter = funkyLetters[letter]
+		console.log(funkyLetter);
+		if(funkyLetter) return funkyLetter;
+		// if there's not, check to see if there's a lowercase version
+		funkyLetter = funkyLetters[letter.toLowerCase()];
+		if(funkyLetter) return funkyLetter;
+		// if nothing, return regular letter
+		return letter;
 	},
   unable(letter, index) {},
 };
