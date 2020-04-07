dbd55cdf7ff8e3579ce314f7483565d3a0ba1d48
Nick-Hericks <nickhericks@gmail.com>
2020-04-07 09:16:58 -0500

    Add sarcastic filter

--- text.js
+++ text.js
@@ -2,16 +2,27 @@ const textarea = document.querySelector("[name='text']");
 const result = document.querySelector('.result');
 const filterInputs = document.querySelectorAll("[name='filter']");
 
-console.log(textarea);
-console.log(result);
-console.log(filterInputs);
 
+const filters = {
+  sarcastic(letter, index) {
+    if (index % 2 == 0) {
+      // if even return uppercase letter
+      return letter.toUpperCase();
+    }
+    // else return lowercase letter
+    return letter.toLowerCase();
+  },
+  funky() {},
+  unable() {},
+};
 
 
 function transformText(text) {
-	result.textContent = text;
+	// take text and loop over each letter
+	const mod = Array.from(text).map(filters.sarcastic);
+	result.textContent = mod.join('');
 }
 
-transformText(textarea.value);
 
+transformText(textarea.value);
 textarea.addEventListener('input', e => transformText(e.target.value));
