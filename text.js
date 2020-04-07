8beed84ea92d2acef67af8f3f8a88aeaf2b424f8
Nick-Hericks <nickhericks@gmail.com>
2020-04-07 09:30:43 -0500

    Add filter functionality

--- text.js
+++ text.js
@@ -1,6 +1,11 @@
 const textarea = document.querySelector("[name='text']");
 const result = document.querySelector('.result');
-const filterInputs = document.querySelectorAll("[name='filter']");
+const filterInputs = Array.from(document.querySelectorAll("[name='filter']"));
+
+
+const funkyLetters = {
+  '-': '₋', '!': 'ᵎ', '?': 'ˀ', '(': '⁽', ')': '₎', '+': '⁺', '=': '₌', '0': '⁰', '1': '₁', '2': '²', '4': '₄', '5': '₅', '6': '₆', '7': '⁷', '8': '⁸', '9': '⁹', a: 'ᵃ', A: 'ᴬ', B: 'ᴮ', b: 'ᵦ', C: '𝒸', d: 'ᵈ', D: 'ᴰ', e: 'ₑ', E: 'ᴱ', f: '𝒻', F: 'ᶠ', g: 'ᵍ', G: 'ᴳ', h: 'ʰ', H: 'ₕ', I: 'ᵢ', i: 'ᵢ', j: 'ʲ', J: 'ᴶ', K: 'ₖ', k: 'ₖ', l: 'ˡ', L: 'ᴸ', m: 'ᵐ', M: 'ₘ', n: 'ₙ', N: 'ᴺ', o: 'ᵒ', O: 'ᴼ', p: 'ᵖ', P: 'ᴾ', Q: 'ᵠ', q: 'ᑫ', r: 'ʳ', R: 'ᵣ', S: 'ˢ', s: 'ˢ', t: 'ᵗ', T: 'ₜ', u: 'ᵘ', U: 'ᵤ', v: 'ᵛ', V: 'ᵥ', w: '𝓌', W: 'ʷ', x: 'ˣ', X: 'ˣ', y: 'y', Y: 'Y', z: '𝓏', Z: 'ᶻ'
+};
 
 
 const filters = {
@@ -12,14 +17,20 @@ const filters = {
     // else return lowercase letter
     return letter.toLowerCase();
   },
-  funky() {},
-  unable() {},
+  funky(letter) {
+
+
+		
+	},
+  unable(letter, index) {},
 };
 
 
 function transformText(text) {
+	// find selected filter on page
+	const filter = filterInputs.find(input => input.checked).value;
 	// take text and loop over each letter
-	const mod = Array.from(text).map(filters.sarcastic);
+	const mod = Array.from(text).map(filters[filter]);
 	result.textContent = mod.join('');
 }
 
